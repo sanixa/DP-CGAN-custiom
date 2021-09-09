17a7138cd9ca755a8f95109400291022ca670617
sanixa <cegg12345678@yahoo.com.tw>
2021-09-09 13:46:15 +0800

    twcc

--- mnist.sh
+++ mnist.sh
@@ -1,5 +1,8 @@
-CUDA_VISIBLE_DEVICES=5 python mnist_torch2.py --noise 0.11145
-CUDA_VISIBLE_DEVICES=5 python mnist_torch2.py --noise 0.242 --clip 1 --lr 5-4
-#CUDA_VISIBLE_DEVICES=0 python mnist_torch2.py --noise 0.437
-CUDA_VISIBLE_DEVICES=5 python mnist_torch2.py --noise 0.95
-CUDA_VISIBLE_DEVICES=5 python mnist_torch2.py --noise 0
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name eps_inf_z32 --noise 0
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name eps_1_z32 --noise 14.5
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name eps_10_z32 --noise 1.45
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name eps_100_z32 --noise 0.531
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name eps_1000_z32 --noise 0.41
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name n_01_z32 --noise 0.1
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name n_02_z32 --noise 0.2
+CUDA_VISIBLE_DEVICES=0 python mnist_torch3.py --g_dim 32 --exp_name n_001_z32 --noise 0.01
